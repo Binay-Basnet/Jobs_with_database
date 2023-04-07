55f63b9946d820488090e5e64998ab73a0ca4dae
Binay Basnet <99240856+Binay-Basnet@users.noreply.github.com>
2023-04-07 16:12:42 +0000

    api changes and commits

--- app.py
+++ app.py
@@ -1,11 +1,32 @@
-from flask import Flask
+from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
+
+Jobs = [{
+  'id': 1,
+  'title': 'Data Analyst',
+  'location': 'Bengaluru',
+  'salary': 1000000
+}, {
+  "id": 2,
+  "title": "Data Scientist",
+  "location": "New York, NY",
+  "salary": 110000
+}, {
+  "id": 3,
+  "title": "Marketing Manager",
+  "location": "Los Angeles, CA",
+}]
 
 
 @app.route('/')
 def index():
-  return "<h1>this is it</h1>"
+  return render_template('home.html', jobs=Jobs, company_name='Binay Blasters')
+
+
+@app.route('/api/jobs')
+def listjobs():
+  return jsonify(Jobs)
 
 
 if __name__ == '__main__':
